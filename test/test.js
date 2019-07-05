610b08cef9cdd08505965855ff3ffe115fbcedac
jecsham <jecsham27@gmail.com>
2019-07-05 00:45:20 -0500

    test added

---
+++ test/test.js
@@ -0,0 +1,37 @@
+"use strict"
+const hexy = require("../");
+const test = require('tape');
+
+test("hexy", t => {
+    
+    /* hexToStr */
+    t.equal(hexy.hexToStr("436176652053746f7279"), "Cave Story");
+    t.equal(hexy.hexToStr("436176652053746F7279"), "Cave Story");
+    t.equal(hexy.hexToStr("43 61 76 65 20 53 74 6f 72 79"), "Cave Story");
+    t.equal(hexy.hexToStr("43 61 76 65 20 53 74 6F 72 79"), "Cave Story");
+
+    t.notEqual(hexy.hexToStr("436176652053746f7279"), "cave story");
+    t.notEqual(hexy.hexToStr("436176652053746F7279"), "cave story");
+    t.notEqual(hexy.hexToStr("43 61 76 65 20 53 74 6f 72 79"), "cave story");
+    t.notEqual(hexy.hexToStr("43 61 76 65 20 53 74 6F 72 79"), "cave story");
+
+    /* strToHex */
+    t.equal(hexy.strToHex("Cave Story"), "436176652053746f7279");
+    t.equal(hexy.strToHex("Cave Story", { gropued: true }), "436176652053746f7279");
+    t.equal(hexy.strToHex("Cave Story", { gropued: true, uppercase: true }), "436176652053746F7279");
+    
+    t.notEqual(hexy.strToHex("cave story"), "436176652053746f7279");
+    t.notEqual(hexy.strToHex("cave story", { gropued: true }), "436176652053746f7279");
+    t.notEqual(hexy.strToHex("cave story", { gropued: true, uppercase: true }), "436176652053746F7279");
+
+    /* isHex */
+    t.ok(hexy.isHex("436176652053746f7279"));
+    t.ok(hexy.isHex("436176652053746F7279"));
+    t.ok(hexy.isHex("43 61 76 65 20 53 74 6f 72 79"));
+    t.ok(hexy.isHex("43 61 76 65 20 53 74 6F 72 79"));
+    
+    t.notOk(hexy.isHex("436176652053746f727"));
+    t.notOk(hexy.isHex("43 61 76 65 20 53 74 6F 72 7"));
+
+    t.end();
+})
