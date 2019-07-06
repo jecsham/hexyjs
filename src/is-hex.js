3b08dcee66cea33b636ef6d96b17fc7070adcfe6
jecsham <jecsham27@gmail.com>
2019-07-05 19:50:44 -0500

    isHex - strict option added

--- src/is-hex.js
+++ src/is-hex.js
@@ -1,9 +1,12 @@
 "use strict"
-module.exports = hex => {
+module.exports = (hex, stric) => {
     hex = hex.replace(/ /g, "");
-    if (hex.length % 2 === 0) {
-        return typeof hex === "string" &&
+    let val = typeof hex === "string" &&
         (hex.match(/([0-9]|[a-f])/gim) || []).length === hex.length;
+    if (stric == false) {
+        return val;
+    } else if (hex.length % 2 === 0) {
+        return val;
     }
     return false;
 }
