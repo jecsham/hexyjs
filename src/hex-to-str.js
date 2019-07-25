7ed847cf0797eb6e6cf1a4241387539ff01a55b3
jecsham <jecsham27@gmail.com>
2019-07-24 20:05:48 -0500

    hext-to-str improved

--- src/hex-to-str.js
+++ src/hex-to-str.js
@@ -2,9 +2,10 @@
 const isHex = require("./is-hex");
 module.exports = hex => {
     const finalHex = hex.replace(/ /g, "");
-
     if (isHex(finalHex)) {
-        const str = decodeURIComponent(finalHex.replace(/(..)/g, "%$1"));
+        let str = "";
+        for (let i = 0; i < finalHex.length; i += 2)
+            str += String.fromCharCode(parseInt(finalHex.substr(i, 2), 16));
         return str;
     }
     return false;
