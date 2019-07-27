df4be4069519daf788e43ccf3bd24b77f179d86e
jecsham <jecsham27@gmail.com>
2019-07-27 18:35:15 -0500

    hex-to-string - Now the function can decode hexadecimal string with any lenght

--- src/hex-to-str.js
+++ src/hex-to-str.js
@@ -2,7 +2,7 @@
 const isHex = require("./is-hex");
 module.exports = hex => {
     const finalHex = hex.replace(/ /g, "");
-    if (isHex(finalHex)) {
+    if (isHex(finalHex, false)) {
         let str = "";
         for (let i = 0; i < finalHex.length; i += 2)
             str += String.fromCharCode(parseInt(finalHex.substr(i, 2), 16));
